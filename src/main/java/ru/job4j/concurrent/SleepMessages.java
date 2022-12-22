fb6b5cd04fd8b805d382e224258cde8ae52b1154
Aleksandr Semeikin <sensem@yandex.ru>
2022-12-22 13:54:57 +0300

    ThreadSleep

---
+++ src/main/java/ru/job4j/concurrent/SleepMessages.java
@@ -0,0 +1,22 @@
+package ru.job4j.concurrent;
+
+public class SleepMessages {
+    public static void main(String[] args)
+            throws InterruptedException {
+        String[] importantInfo = {
+                "Mares eat oats",
+                "Does eat oats",
+                "Little lambs eat ivy",
+                "A kid will eat ivy too"
+        };
+        System.out.println(Thread.currentThread().getName());
+        for (int i = 0;
+             i < importantInfo.length;
+             i++) {
+            //Pause for 4 seconds
+            Thread.sleep(4000);
+            //Print a message
+            System.out.println(importantInfo[i]);
+        }
+    }
+}
