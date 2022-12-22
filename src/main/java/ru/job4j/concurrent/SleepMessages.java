26f0a52828c64cfc063ab75ca2e415e0dcd4552a
Aleksandr Semeikin <sensem@yandex.ru>
2022-12-22 14:14:58 +0300

    ThreadSleep

--- src/main/java/ru/job4j/concurrent/SleepMessages.java
+++ src/main/java/ru/job4j/concurrent/SleepMessages.java
@@ -13,9 +13,9 @@ public class SleepMessages {
         for (int i = 0;
              i < importantInfo.length;
              i++) {
-            //Pause for 4 seconds
+            /*Pause for 4 seconds*/
             Thread.sleep(4000);
-            //Print a message
+            /*Print a message*/
             System.out.println(importantInfo[i]);
         }
     }
