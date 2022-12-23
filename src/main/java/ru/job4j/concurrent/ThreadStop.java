17cf8ea539f1cc8dae1539f0a924c431046c84eb
Aleksandr Semeikin <sensem@yandex.ru>
2022-12-23 14:07:01 +0300

    Interruption of blocked thread

--- src/main/java/ru/job4j/concurrent/ThreadStop.java
+++ src/main/java/ru/job4j/concurrent/ThreadStop.java
@@ -1,14 +1,13 @@
 package ru.job4j.concurrent;
 
+/**
+ * Interruption of blocked thread
+ */
 public class ThreadStop {
-    public static void main(String[] args) {
-        try {
-            Thread progress = new Thread(new ConsoleProgress());
-            progress.start();
-            Thread.sleep(3000); /*второй поток выполняется в течение этого времени*/
-            progress.interrupt();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(3000); /*второй поток выполняется в течение этого времени*/
+        progress.interrupt();
     }
 }
